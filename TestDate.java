d67861ec400955d87c6419eb7575f073e27258f7
rencho13 <stephone13@gmail.com>
2019-09-28 23:41:08 +0200

    challenge ok

--- TestDate.java
+++ TestDate.java
@@ -1,23 +1,26 @@
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.Date;
-import java.util.Scanner; 
+import java.util.Scanner;
 
 class TestDate {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please fill a day (1-31) :");
-        int day = sc.nextInt();
-        System.out.println("Please fill a month (1-12) :");
-        int month = sc.nextInt();
-        System.out.println("Please fill a year :");
-        int year = sc.nextInt();
-        sc.close();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please fill a day (1-31) :");
+		int day = sc.nextInt();
+		System.out.println("Please fill a month (1-12) :");
+		int month = sc.nextInt();
+		System.out.println("Please fill a year :");
+		int year = sc.nextInt();
+		sc.close();
 
-        // TODO : your code after this line
-        
-    }
+		SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy");
+		Calendar calendar = new GregorianCalendar(year, month - 1, day);// -1 car janvier commence a 0
+
+		System.out.println("The date is : " + format.format(calendar.getTime()));
+		System.out.println("The day of week is : " + calendar.get(Calendar.DAY_OF_WEEK));
+
+	}
 }
